16f200344e5152c220309d91a512846a489696dd
rrzaman <rayyan.rz5@gmail.com>
2026-05-01 00:19:27 -0600

    Fixed Cognito client ID and pool ID preventing login.

--- terraform/modules/auth/main.tf
+++ terraform/modules/auth/main.tf
@@ -19,13 +19,13 @@ resource "aws_cognito_user_pool" "main" {
 }
 
 resource "aws_cognito_user_pool_client" "web" {
-  name            = "${var.project_name}-web"
-  user_pool_id    = aws_cognito_user_pool.main.id
-  generate_secret = false
+  name         = "${var.project_name}-web"
+  user_pool_id = aws_cognito_user_pool.main.id
 
+  generate_secret                      = false
+  allowed_oauth_flows_user_pool_client = true
   allowed_oauth_flows                  = ["code"]
   allowed_oauth_scopes                 = ["openid", "email", "profile"]
-  allowed_oauth_flows_user_pool_client = true
   supported_identity_providers         = ["COGNITO"]
 
   callback_urls = var.callback_urls
@@ -34,5 +34,15 @@ resource "aws_cognito_user_pool_client" "web" {
   explicit_auth_flows = [
     "ALLOW_USER_PASSWORD_AUTH",
     "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_SRP_AUTH",
   ]
+
+  enable_token_revocation       = true
+  prevent_user_existence_errors = "ENABLED"
+}
+
+resource "aws_cognito_user_pool_domain" "main" {
+  domain                = "ca-west-1s938ojszp"
+  user_pool_id          = aws_cognito_user_pool.main.id
+  managed_login_version = 2
 }
